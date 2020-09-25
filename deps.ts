8598e1a2b5dff841bd1dafce0f1feed4430910e7
Benjamin Fischer <c4spar@gmx.de>
2020-09-25 23:44:37 +0200

    ref: refactor progress event

--- deps.ts
+++ deps.ts
@@ -3,10 +3,6 @@ export {
 } from "https://deno.land/std@0.70.0/testing/asserts.ts";
 
 export {
-  readLines,
-} from "https://deno.land/std@0.70.0/io/bufio.ts";
-
-export {
   blue,
   bold,
   red,
