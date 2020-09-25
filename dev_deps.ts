bcea9ac2d30ff52b3ce4ef9acd8dfaad8fc5d327
Benjamin Fischer <c4spar@gmx.de>
2020-09-26 00:45:21 +0200

    chore(upgrade): deno/std to v0.71.0

--- dev_deps.ts
+++ dev_deps.ts
@@ -3,20 +3,20 @@ export {
   assertNotEquals,
   assertThrows,
   assertThrowsAsync,
-} from "https://deno.land/std@0.70.0/testing/asserts.ts";
+} from "https://deno.land/std@0.71.0/testing/asserts.ts";
 
 export {
   exists,
-} from "https://deno.land/std@0.70.0/fs/exists.ts";
+} from "https://deno.land/std@0.71.0/fs/exists.ts";
 
 export {
   ensureDir,
-} from "https://deno.land/std@0.70.0/fs/ensure_dir.ts";
+} from "https://deno.land/std@0.71.0/fs/ensure_dir.ts";
 
 export {
   dirname,
-} from "https://deno.land/std@0.70.0/path/mod.ts";
+} from "https://deno.land/std@0.71.0/path/mod.ts";
 
 export {
   MuxAsyncIterator,
-} from "https://deno.land/std@0.70.0/async/mux_async_iterator.ts";
+} from "https://deno.land/std@0.71.0/async/mux_async_iterator.ts";
