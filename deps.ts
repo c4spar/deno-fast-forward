11cd5a105bb11db1e33d20815da5146c1fcb8baf
Benjamin Fischer <c4spar@gmx.de>
2020-09-26 00:45:21 +0200

    chore(upgrade): deno/std to v0.71.0

--- deps.ts
+++ deps.ts
@@ -1,10 +1,10 @@
 export {
   AssertionError,
-} from "https://deno.land/std@0.70.0/testing/asserts.ts";
+} from "https://deno.land/std@0.71.0/testing/asserts.ts";
 
 export {
   blue,
   bold,
   red,
   stripColor,
-} from "https://deno.land/std@0.70.0/fmt/colors.ts";
+} from "https://deno.land/std@0.71.0/fmt/colors.ts";
