0be4b631f5be98e632d7081f2dbc9694f636e7d2
Benjamin Fischer <c4spar@gmx.de>
2021-03-28 23:41:08 +0200

    chore(upgrade): deno/std v0.91.0

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { blue, bold, red } from "https://deno.land/std@0.79.0/fmt/colors.ts";
-export { deferred } from "https://deno.land/std@0.79.0/async/deferred.ts";
-export type { Deferred } from "https://deno.land/std@0.79.0/async/deferred.ts";
+export { blue, bold, red } from "https://deno.land/std@0.91.0/fmt/colors.ts";
+export { deferred } from "https://deno.land/std@0.91.0/async/deferred.ts";
+export type { Deferred } from "https://deno.land/std@0.91.0/async/deferred.ts";
