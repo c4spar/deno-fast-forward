33c43a349926ec0ed991dd0ea9a00a3f4a33c163
Benjamin Fischer <c4spar@gmx.de>
2021-07-28 22:25:25 +0200

    chore(upgrade): deno/std v0.103.0

--- dev_deps.ts
+++ dev_deps.ts
@@ -4,16 +4,16 @@ export {
   assertNotEquals,
   assertThrows,
   assertThrowsAsync,
-} from "https://deno.land/std@0.93.0/testing/asserts.ts";
+} from "https://deno.land/std@0.103.0/testing/asserts.ts";
 
-export { exists } from "https://deno.land/std@0.93.0/fs/exists.ts";
+export { exists } from "https://deno.land/std@0.103.0/fs/exists.ts";
 
-export { ensureDir } from "https://deno.land/std@0.93.0/fs/ensure_dir.ts";
+export { ensureDir } from "https://deno.land/std@0.103.0/fs/ensure_dir.ts";
 
-export { dirname, fromFileUrl } from "https://deno.land/std@0.93.0/path/mod.ts";
+export { dirname, fromFileUrl } from "https://deno.land/std@0.103.0/path/mod.ts";
 
 export {
   MuxAsyncIterator,
-} from "https://deno.land/std@0.93.0/async/mux_async_iterator.ts";
+} from "https://deno.land/std@0.103.0/async/mux_async_iterator.ts";
 
-export { stripColor } from "https://deno.land/std@0.93.0/fmt/colors.ts";
+export { stripColor } from "https://deno.land/std@0.103.0/fmt/colors.ts";
