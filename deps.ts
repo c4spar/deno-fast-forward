835f343d1ad8cb46c8a788b24289405e7475b86e
Benjamin Fischer <61995275+c4spar@users.noreply.github.com>
2021-12-03 01:39:41 +0100

    chore(upgrade): deno/std v0.116.0 (#16)

--- deps.ts
+++ deps.ts
@@ -1,4 +1,7 @@
-export { blue, bold, red } from "https://deno.land/std@0.103.0/fmt/colors.ts";
-export { deferred } from "https://deno.land/std@0.103.0/async/deferred.ts";
-export { copy, iter } from "https://deno.land/std@0.103.0/io/util.ts";
-export type { Deferred } from "https://deno.land/std@0.103.0/async/deferred.ts";
+export { blue, bold, red } from "https://deno.land/std@0.116.0/fmt/colors.ts";
+export { deferred } from "https://deno.land/std@0.116.0/async/deferred.ts";
+export {
+  copy,
+  iterateReader,
+} from "https://deno.land/std@0.116.0/streams/mod.ts";
+export type { Deferred } from "https://deno.land/std@0.116.0/async/deferred.ts";
--- dev_deps.ts
+++ dev_deps.ts
@@ -2,21 +2,21 @@ export {
   assertEquals,
   AssertionError,
   assertNotEquals,
+  assertRejects,
   assertThrows,
-  assertThrowsAsync,
-} from "https://deno.land/std@0.103.0/testing/asserts.ts";
+} from "https://deno.land/std@0.116.0/testing/asserts.ts";
 
-export { exists } from "https://deno.land/std@0.103.0/fs/exists.ts";
+export { exists } from "https://deno.land/std@0.116.0/fs/exists.ts";
 
-export { ensureDir } from "https://deno.land/std@0.103.0/fs/ensure_dir.ts";
+export { ensureDir } from "https://deno.land/std@0.116.0/fs/ensure_dir.ts";
 
 export {
   dirname,
   fromFileUrl,
-} from "https://deno.land/std@0.103.0/path/mod.ts";
+} from "https://deno.land/std@0.116.0/path/mod.ts";
 
 export {
   MuxAsyncIterator,
-} from "https://deno.land/std@0.103.0/async/mux_async_iterator.ts";
+} from "https://deno.land/std@0.116.0/async/mux_async_iterator.ts";
 
-export { stripColor } from "https://deno.land/std@0.103.0/fmt/colors.ts";
+export { stripColor } from "https://deno.land/std@0.116.0/fmt/colors.ts";
